ced9fdbc7e90405f92013b394d53e10f25ec1405
laks0 <laksjoaquin@gmail.com>
2021-04-03 19:15:40 -0300

    pause/stop and reset buttons

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ class App extends React.Component{
 		this.whitePass = this.whitePass.bind(this);
 		this.blackPass = this.blackPass.bind(this);
 		this.secondPassed = this.secondPassed.bind(this);
+		this.playpause = this.playpause.bind(this);
+		this.pause = this.pause.bind(this);
+		this.reset = this.reset.bind(this);
 	}
 
 	interval = null;
@@ -54,15 +57,47 @@ class App extends React.Component{
 		});
 	}
 
+	playpause() {
+		if (this.state.running) {
+			this.pause();
+		} else {
+			this.startTimer(this.state.turn);
+		}
+	}
+
+	pause() {
+		clearInterval(this.interval);
+		this.setState({running: false});
+	}
+
+	reset() {
+		this.pause();
+		this.setState({
+			times: [startTime * 60, startTime * 60],
+			turn: 0, // 0 = white, 1 = black
+		});
+	}
+
 	render() {
 		return (
 			<div className="App">
+
+				<link rel="stylesheet" 
+ href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+ integrity="sha384- 
+ 50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" 
+					crossorigin="anonymous"/>
+
 				<div id="clock-container">
 					<button onClick={this.whitePass} className="passButton" disabled={this.state.turn != 0}/>
 
 					<Display key="whiteDisplay" color="white" timeLeft={this.state.times[0]}/>
 
-					<p>{startTime + " | " + this.state.increment}</p>
+						<p>
+							<button onClick={this.reset} className="ui fa fa-backward"/> 
+							{startTime + " | " + this.state.increment} 
+							<button onClick={this.playpause} className={"ui fa fa-" + (this.state.running ? "stop" : "play")}/>
+						</p>
 
 					<Display key="blackDsiplay" color="black" timeLeft={this.state.times[1]}/>
 
