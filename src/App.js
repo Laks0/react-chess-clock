5377f9a8d704bef732b38d5a00eaed266578f02a
laks0 <laksjoaquin@gmail.com>
2021-04-03 16:25:36 -0300

    clocks now count down

--- src/App.js
+++ src/App.js
@@ -10,20 +10,48 @@ class App extends React.Component{
 
 		this.state = {
 			increment: 10,
-			whiteTime: startTime * 60,
-			blackTime: startTime * 60,
+			times: [startTime * 60, startTime * 60],
 			turn: 0, // 0 = white, 1 = black
 			running: false,
 		};
 
 		this.startTimer = this.startTimer.bind(this);
+		this.whitePass = this.whitePass.bind(this);
+		this.blackPass = this.blackPass.bind(this);
+		this.secondPassed = this.secondPassed.bind(this);
 	}
 
-	whitePass = () => {this.startTimer(1)};
-	blackPass = () => {this.startTimer(0)};
+	interval = null;
+
+	whitePass() {this.startTimer(1)};
+	blackPass() {this.startTimer(0)};
 
 	startTimer(turn) { 
-		this.setState({turn: turn});
+		this.setState((state) => {
+			let newTimes = state.times;
+			if ( state.running ) {
+				newTimes[state.turn] += state.increment / 2;
+			}
+
+			return {
+				turn: turn,
+				running: true,
+				times: newTimes,
+			};
+		});
+
+		clearInterval(this.interval);
+		this.interval = setInterval(this.secondPassed, 1000);
+	}
+
+	secondPassed() {
+		this.setState( state => {
+			let newTimes = [...state.times ];
+
+			newTimes[state.turn] -= 1;
+
+			return {times: newTimes};
+		});
 	}
 
 	render() {
@@ -32,11 +60,11 @@ class App extends React.Component{
 				<div id="clock-container">
 					<button onClick={this.whitePass} className="passButton" disabled={this.state.turn != 0}/>
 
-					<Display color="white" timeLeft={this.state.whiteTime}/>
+					<Display key="whiteDisplay" color="white" timeLeft={this.state.times[0]}/>
 
 					<p>{startTime + " | " + this.state.increment}</p>
 
-					<Display color="black" timeLeft={this.state.blackTime}/>
+					<Display key="blackDsiplay" color="black" timeLeft={this.state.times[1]}/>
 
 					<button onClick={this.blackPass} className="passButton" disabled={this.state.turn != 1}/>
 				</div>
